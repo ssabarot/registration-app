c79e2ee08e91bef1300a121f30d54155f3a9f487
ssabarot <nephtys64@hotmail.fr>
2023-10-03 10:55:08 +0200

    feat(): javadoc restcontroller

--- src/main/java/ssabarot/springboot/registrationapp/controller/UserController.java
+++ src/main/java/ssabarot/springboot/registrationapp/controller/UserController.java
@@ -23,11 +23,22 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     * GET method to retrieve a user by id
+     *
+     * @param id of the user we are looking for
+     * @return a {@link UserDto} corresponding to the id or a ResourceNotFoundException if no user was found
+     */
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDto> findUserById(@PathVariable("id") Long id) {
         return userService.findUserById(id).map(ResponseEntity::ok).orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found."));
     }
 
+    /**
+     * GET method to retrieve all users
+     *
+     * @return a {@link List<UserDto>} corresponding to the list of all users or a NO_CONTENT status if the list is empty
+     */
     @GetMapping("")
     public ResponseEntity<List<UserDto>> getAllUsers() {
         List<UserDto> userDtosList = new ArrayList<>(userService.findAllUsers());
@@ -39,6 +50,12 @@ public class UserController {
         return new ResponseEntity<>(userDtosList, HttpStatus.OK);
     }
 
+    /**
+     * POST method to create a new user
+     *
+     * @param userDto {@link UserDto} to create
+     * @return the {@link UserDto} if successfully created or a 500 status response if userDto is invalid
+     */
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto userDto) {
         return ResponseEntity.ok(userService.createUser(userDto));
